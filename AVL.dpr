cedd7522d9c7ab3bb90766f9321815c75e8b213f
OzihcS <smile.we.ua@gmail.com>
2015-12-22 23:51:00 +0200

    Initial commit

---
+++ AVL.dpr
@@ -0,0 +1,15 @@
+//////////////////////////
+//    theme: AVL Tree   //
+// author: A.Piontkowski//
+//////////////////////////
+
+program AVL;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+end.
